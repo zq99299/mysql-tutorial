2e01406c414e64f1d90dcbf9ec2eaa2befedde56
zq99299 <99299684@qq.com>
2020-04-21 23:25:56 +0800

    调整章节

--- docs/imooc-mysql8/index.js
+++ docs/imooc-mysql8/index.js
@@ -6,11 +6,11 @@ module.exports = () => {
     '03/',
     '04/',
     {
-      title: '玩转 SQL 之 日常工作',
+      title: '日常工作 DCL、DDL',
       children: [
         '05/',
         '05/01.md',
-        '01/02.md'
+        '05/02.md'
       ]
     }
   ]
