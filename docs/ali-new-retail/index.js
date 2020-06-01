f461e64117be139b3971234288c35758e3259e7d
zq99299 <99299684@qq.com>
2020-06-01 17:48:05 +0800

    修复错误的高亮语言，侧边栏编排

--- docs/ali-new-retail/index.js
+++ docs/ali-new-retail/index.js
@@ -2,5 +2,20 @@ module.exports = () => {
   return [
     '',
     '01/',
+    {
+      title: '前置准备',
+      children: [
+        '02/',
+        '02/01.md',
+        '02/02.md'
+      ]
+    }, {
+      title: '前导知识',
+      children: [
+        '03/',
+        '03/01.md',
+        '03/02.md'
+      ]
+    }
   ]
 }
